14bcce195ea9951e7d98c92ac704a89f396f7f1c
medzumi <pkkudrin@gmail.com>
2022-05-22 18:15:49 +0300

    add null check method for unity

---
+++ Runtime/Extensions/UnityCodeBeautifyExtensions.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Utilities.CodeExtensions;
+
+namespace Utilities.Unity.Extensions
+{
+    public static class UnityCodeBeautifyExtensions
+    {
+        public static bool IsNullInUnity(this object obj)
+        {
+            if (obj is Object uObj)
+            {
+                return !uObj;
+            }
+            else
+            {
+                return obj.IsNull();
+            }
+        }
+
+        public static bool IsNotNullInUnity(this object obj)
+        {
+            if (obj is Object uObj)
+            {
+                return uObj;
+            }
+            else
+            {
+                return obj.IsNotNull();
+            }
+        }
+    }
+}
